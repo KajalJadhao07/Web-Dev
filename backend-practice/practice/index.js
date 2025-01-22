1c1a8d6c8194b6c4a59ffaa62118f317547198ea
webdev <kajaljadhao16@gmail.com>
2025-01-22 22:37:32 +0530

    Did some changes to fix the route and remove some unnecessary details

--- backend-practice/practice/index.js
+++ backend-practice/practice/index.js
@@ -1,33 +1,14 @@
-const express = require("express");   // require express package
+const express = require("express"); 
 let app = express();
 
 app.set("view engine", "ejs");
 
-let port = 8080;     // initialize the port
+let port = 8080;    
 
-app.listen(port , ()=>{        //listening to the port
+app.listen(port , ()=>{    
     console.log("listening to port", port);
 });
 
 app.get("/", (req, res)=>{
     res.render("home.ejs");
-});
-
-app.get("/rolldice", (req,res)=>{     // data i.e diceVal come from database and we want to send it to the roll_dice file 
-    let diceVal = Math.floor(Math.random()*6)+1;
-    res.render("roll_dice.ejs", {diceVal})
-});
-
-
-
-//For instagram activity
-app.get("/ig/:username", (req,res)=>{
-    let instadata = require("./data.json");
-    let { username } = req.params;
-    const data = instadata[username];
-    if(data){
-        res.render("instagram.ejs", { data })
-    }else{
-        res.render("error.ejs");
-    }
 });
