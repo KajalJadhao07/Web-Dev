70ee9d1afb71f098f803833baf0661594ab4107a
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-06-11 08:31:17 +0530

    It is the main file of the react which contains all the functions or the callback

---
+++ React/React-Practice/basic-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ProductList from './ProductList.jsx';
+import Title from "./Title.jsx";
+import MsgBoxList from './MsgBoxList.jsx';
+
+function App() {
+  return (
+    // <div>
+    // <Title />
+    // <button>Hello World!</button>
+    // </div>
+    // <ProductList />
+
+    <MsgBoxList />
+  )
+}
+
+export default App;
