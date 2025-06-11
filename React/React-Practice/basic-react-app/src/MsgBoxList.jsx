5cbbcd6a21bad3f5caf004f3d42201a8c138ed0d
KajalJadhao07 <kajaljadhao16@gmail.com>
2025-06-11 08:34:46 +0530

    It contains the list of username and the text color

---
+++ React/React-Practice/basic-react-app/src/MsgBoxList.jsx
@@ -0,0 +1,12 @@
+import MsgBox from "./MsgBox.jsx"
+
+function MsgBoxList(){
+    return(
+        <>
+            <MsgBox username = "Sarang" textcolor = "skyblue"/>
+            <MsgBox username = "Kajal" textcolor = "hotpink"/>
+        </>
+    )
+}
+
+export default MsgBoxList;
